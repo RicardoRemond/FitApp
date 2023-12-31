6da5f7215e9682c9ec6fc3ea716103c2566644eb
Ricardo <ricardo.remond.pro@gmail.com>
2023-12-31 18:52:14 +0100

    login&register css en cours

--- PAGE/login.php
+++ PAGE/login.php
@@ -1,55 +1,35 @@
-<?php
-// Paramètres de connexion à la base de données
-$servername = "localhost"; // Adresse du serveur de base de données
-$username = ""; // Nom d'utilisateur de la base de données
-$password = ""; // Mot de passe de la base de données
-$dbname = "fitapp"; // Nom de la base de données
-
-try {
-    // Création d'une connexion PDO
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
-    // Configuration des attributs de PDO pour signaler les erreurs
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Votre logique de traitement de base de données ici
-} catch (PDOException $e) {
-    // En cas d'erreur de connexion, affichez l'erreur
-    echo "Erreur de connexion : " . $e->getMessage();
-}
-?>
-
-
-<?php
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Inclure le script de traitement du formulaire de connexion (login_process.php)
-    include 'login_process.php';
-}
-?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@100;300;400;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./assets/style/login.css">
     <title>Connexion</title>
 </head>
 
 <body>
-
+    <img src="./assets/img/logoTIDR.png" alt="Logo App">
     <form action="login_process.php" method="POST">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
+        <label for="identifiant">Identifiant:</label>
+        <input type="identifiant" id="identifiant" name="identifiant" required><br><br>
 
         <label for="password">Mot de passe:</label>
         <input type="password" id="password" name="password" required><br><br>
 
-        <input type="submit" value="Se connecter">
+        <div class="connexionEtMDP">
+            <input type="submit" value="Se connecter">
+            <input type="submit" value="Mot de passe oublié">
+        </div>
+
+        <p>Pas encore inscrit ? <a href="register.php">Inscrivez-vous ici</a>.</p>
+
     </form>
 
-    <p>Pas encore inscrit ? <a href="register.php">Inscrivez-vous ici</a>.</p>
 
 </body>
 
--- PAGE/register.php
+++ PAGE/register.php
@@ -1,43 +1,18 @@
-<?php
-// Paramètres de connexion à la base de données
-$servername = "localhost"; // Adresse du serveur de base de données
-$username = ""; // Nom d'utilisateur de la base de données
-$password = ""; // Mot de passe de la base de données
-$dbname = "fitapp"; // Nom de la base de données
-
-try {
-    // Création d'une connexion PDO
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
-    // Configuration des attributs de PDO pour signaler les erreurs
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Votre logique de traitement de base de données ici
-} catch (PDOException $e) {
-    // En cas d'erreur de connexion, affichez l'erreur
-    echo "Erreur de connexion : " . $e->getMessage();
-}
-?>
-
-
-<?php
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Inclure le script de traitement du formulaire d'inscription (register_process.php)
-    include 'register_process.php';
-}
-?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@100;300;400;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./assets/style/register.css">
     <title>Inscription</title>
 </head>
 
 <body>
+    <img src="./assets/img/logoTIDR.png" alt="Logo App">
+
     <form action="register_process.php" method="POST">
         <h2>Inscription</h2>
         <label for="firstname">Prénom:</label>
@@ -48,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="email" id="email" name="email" required><br><br>
         <label for="password">Mot de passe:</label>
         <input type="password" id="password" name="password" required><br><br>
-        <!-- Autres champs d'inscription -->
+        <!-- Autres infos d'inscription -->
         <input type="submit" value="S'inscrire">
     </form>
 
